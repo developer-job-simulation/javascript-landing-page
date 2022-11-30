d06f3e57823617aab524bd881a2c11386a53182b
Jared Mercer <jaredthebackpacker@gmail.com>
2022-11-30 12:38:02 -0800

    Fixed ticket #5: [Feature Request] Improve Errors on Signup Form Validation

--- src/script.js
+++ src/script.js
@@ -1,42 +1,52 @@
 const usersTable = [
-  // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
-  { username: "hello@world.com" },
-  { username: "test@user.com" },
-  { username: "email@domain.com" },
+    // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
+    { username: "hello@world.com" },
+    { username: "test@user.com" },
+    { username: "email@domain.com" },
 ];
 let renderSuccess = () => {
-  document.getElementById("success-message").hidden = false;
+    document.getElementById("success-message").hidden = false;
 };
 let renderEmailTakenError = () => {
-  document.getElementById("taken-error-message").hidden = false;
+    document.getElementById("taken-error-message").hidden = false;
 };
 let renderEmailEmptyError = () => {
-  document.getElementById("empty-error-message").hidden = false;
+    document.getElementById("empty-error-message").hidden = false;
 };
 let resetMessage = () => {
-  document.getElementById("success-message").hidden = true;
-  document.getElementById("taken-error-message").hidden = true;
-  document.getElementById("empty-error-message").hidden = true;
+    document.getElementById("success-message").hidden = true;
+    document.getElementById("taken-error-message").hidden = true;
+    document.getElementById("empty-error-message").hidden = true;
 };
 
 addEventListener("submit", (event) => {
-  event.preventDefault();
-  resetMessage();
+    event.preventDefault();
+    resetMessage();
 
-  let email = document.getElementById("email").value;
+    let email = document.getElementById("email").value;
+    if (email === "") {
+        renderEmailEmptyError();
+        return;
+    }
+    const users = [];
 
-  // TODO: Show Correct Status Messages on Signup Form
-  // 1. successful signup
-  // 2. empty email
-  // 3. taken email
-  // 4. repeat email
+    for (let i = 0; i < usersTable.length; i++) {
+        users.push(usersTable[i].username);
+    }
+
+    if (users.includes(email)) {
+        renderEmailTakenError();
+    } else {
+        usersTable.push({ username: email });
+        renderSuccess();
+    }
 });
 
 let toggleNav = () => {
-  var nav = document.getElementById("mobile-nav");
-  if (nav.className.indexOf("show") == -1) {
-    nav.className += " show";
-  } else {
-    nav.className = nav.className.replace(" show", "");
-  }
+    var nav = document.getElementById("mobile-nav");
+    if (nav.className.indexOf("show") == -1) {
+        nav.className += " show";
+    } else {
+        nav.className = nav.className.replace(" show", "");
+    }
 };
