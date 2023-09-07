3d6ec038b68a40d01fa1f1ff34e618560350ec15
Parker Sturtevant <wsturtevant196@gmail.com>
2023-09-07 13:19:54 -0400

    Fixed nav bar button issues and mobile nav rendering issues

--- src/script.js
+++ src/script.js
@@ -1,42 +1,43 @@
 const usersTable = [
-  // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
-  { username: "hello@world.com" },
-  { username: "test@user.com" },
-  { username: "email@domain.com" },
+	// Note: This is a fake table for educational purposes. Never store user credentials in plain text.
+	{ username: 'hello@world.com' },
+	{ username: 'test@user.com' },
+	{ username: 'email@domain.com' },
 ];
 let renderSuccess = () => {
-  document.getElementById("success-message").hidden = false;
+	document.getElementById('success-message').hidden = false;
 };
 let renderEmailTakenError = () => {
-  document.getElementById("taken-error-message").hidden = false;
+	document.getElementById('taken-error-message').hidden = false;
 };
 let renderEmailEmptyError = () => {
-  document.getElementById("empty-error-message").hidden = false;
+	document.getElementById('empty-error-message').hidden = false;
 };
 let resetMessage = () => {
-  document.getElementById("success-message").hidden = true;
-  document.getElementById("taken-error-message").hidden = true;
-  document.getElementById("empty-error-message").hidden = true;
+	document.getElementById('success-message').hidden = true;
+	document.getElementById('taken-error-message').hidden = true;
+	document.getElementById('empty-error-message').hidden = true;
 };
 
-addEventListener("submit", (event) => {
-  event.preventDefault();
-  resetMessage();
+addEventListener('submit', (event) => {
+	event.preventDefault();
+	resetMessage();
 
-  let email = document.getElementById("email").value;
+	let email = document.getElementById('email').value;
 
-  // TODO: Show Correct Status Messages on Signup Form
-  // 1. successful signup
-  // 2. empty email
-  // 3. taken email
-  // 4. repeat email
+	// TODO: Show Correct Status Messages on Signup Form
+	// 1. successful signup
+	// 2. empty email
+	// 3. taken email
+	// 4. repeat email
 });
 
 let toggleNav = () => {
-  var nav = document.getElementById("mobile-nav");
-  if (nav.className.indexOf("show") == -1) {
-    nav.className += " show";
-  } else {
-    nav.className = nav.className.replace(" show", "");
-  }
+	var nav = document.getElementById('mobile-nav');
+	if (nav.className.indexOf('show') == -1) {
+		nav.className += ' show';
+	} else {
+		nav.className = nav.className.replace(' show', '');
+	}
+	console.log('clicked!');
 };
