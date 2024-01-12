cedcbddf05e1f06dff717d176ba4499e6ed04092
Millner Philipp <philipp.millner@outlook.com>
2024-01-12 12:52:08 +0100

    fixed Issues

--- src/script.js
+++ src/script.js
@@ -30,6 +30,15 @@ addEventListener("submit", (event) => {
   // 2. empty email
   // 3. taken email
   // 4. repeat email
+  if (email === "") return renderEmailEmptyError()
+  
+  for (const user of usersTable)
+  {
+    if(user.username === email) return renderEmailTakenError()
+  }
+
+  usersTable.push({username: email})
+  renderSuccess()
 });
 
 let toggleNav = () => {
